09e54f62d270b7092dbfa799f1112bcf64171c04
Chris Wrench <chris.wrench@enable.com>
2017-09-05 09:07:49 +0100

    Initial commit

---
+++ src/DataIntegrityException/DataIntegrityException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Enable.Common
+{
+    /// <summary>
+    /// Represents an attempt that has been made to compromise the integrity of the application data.
+    /// </summary>
+    [Serializable]
+    public class DataIntegrityException : Exception
+    {
+        public DataIntegrityException()
+            : base()
+        {
+        }
+
+        public DataIntegrityException(string message)
+            : base(message)
+        {
+        }
+
+        public DataIntegrityException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected DataIntegrityException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+    }
+}
---
+++ test/DataIntegrityException.Test/DataIntegrityExceptionTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Enable.Common
+{
+    public class DataIntegrityExceptionTests
+    {
+        private readonly Fixture _fixture;
+
+        public DataIntegrityExceptionTests()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public void Should_Be_An_Exception_Type()
+        {
+            // Act
+            var sut = new DataIntegrityException();
+
+            // Assert
+            Assert.IsAssignableFrom<Exception>(sut);
+        }
+
+        [Fact]
+        public void Should_Have_Default_Constructor()
+        {
+            // Act
+            var sut = new DataIntegrityException();
+
+            // Assert
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void Can_Be_Initialised_With_Exception_Message()
+        {
+            // Arrange
+            var message = _fixture.Create<string>();
+
+            // Act
+            var sut = new DataIntegrityException(message);
+
+            // Assert
+            Assert.Equal(message, sut.Message);
+        }
+
+        [Fact]
+        public void Can_Be_Initialised_With_Exception_Message_And_Inner_Exception()
+        {
+            // Arrange
+            var message = _fixture.Create<string>();
+            var innerException = _fixture.Create<Exception>();
+
+            // Act
+            var sut = new DataIntegrityException(message, innerException);
+
+            // Assert
+            Assert.Equal(message, sut.Message);
+            Assert.Equal(innerException, sut.InnerException);
+        }
+
+        [Fact]
+        public void Should_Roundtrip_Message_During_Serialization()
+        {
+            // Arrange
+            var formatter = new BinaryFormatter();
+            var message = _fixture.Create<string>();
+            var sut = new DataIntegrityException(message);
+
+            // Act
+            DataIntegrityException result;
+
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, sut);
+                stream.Position = 0;
+                result = (DataIntegrityException)formatter.Deserialize(stream);
+            }
+
+            // Assert
+            Assert.Equal(message, result.Message);
+        }
+    }
+}
